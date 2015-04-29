887493e9f64015c7684b72f08f72278fd30e53ca
eugen_chyvuchyn <chuvendoil@gmail.com>
2015-04-29 20:31:20 +0300

    first commit

---
+++ someProgs/src/main/java/test/HelloGit.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by Chuvychin on 29.04.2015.
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git :)");
+    }
+}
